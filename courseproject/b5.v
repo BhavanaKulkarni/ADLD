53b8413c76e3d9be472590656c4c929ee062590a
Bhavana <83966554+BhavanaKulkarni@users.noreply.github.com>
2023-05-18 17:28:21 +0530

    Add files via upload

---
+++ courseproject/b5.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:28:25 05/13/2023 
+// Design Name: 
+// Module Name:    b5 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module b5(X, Y, Z);
+      input signed [7:0] X, Y;
+       output signed [15:0] Z;
+       reg signed [15:0] Z;
+       reg [1:0] temp;
+       integer i; 
+       reg E1;
+       reg [7:0] Y1;
+       always @ (X, Y)
+       begin
+       Z = 16'd0;
+       E1 = 1'd0;
+       for (i = 0; i < 8; i = i + 1)
+       begin
+       temp = {X[i], E1};
+		 Y1 = - Y;
+  case (temp)
+       2'd2 : Z [15 : 8] = Z [15 : 8] + Y1;
+       2'd1 : Z [15 : 8] = Z [15 : 8] + Y;
+       default : begin end
+       endcase
+       Z = Z >> 1;
+       
+       Z[15] = Z[14];
+       
+      
+       E1 = X[i];
+           end
+       if (Y == 8'd16)
+      
+      
+           begin
+               Z = - Z;
+           end
+      
+       end
+      
+      //Z={Z,8'b0}; 
+endmodule 
---
+++ courseproject/difference.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:07:29 05/13/2023 
+// Design Name: 
+// Module Name:    difference 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module difference(
+    input [15:0] A,
+    input [15:0] B,
+    output reg [15:0] diff
+);
+
+reg [15:0] high;
+reg [15:0] low;
+
+// Compare A and B to determine the higher and lower numbers
+always @* begin
+    if (A >= B) begin
+        high = A;
+        low = B;
+    end else begin
+        high = B;
+        low = A;
+    end
+end
+
+// Calculate the difference between the higher and lower numbers
+always @* begin
+    diff = high - low;
+end
+
+endmodule
