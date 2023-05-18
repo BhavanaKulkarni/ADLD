4fda3ce2af9671331e27b02072d83cd1b32c384b
Bhavana <83966554+BhavanaKulkarni@users.noreply.github.com>
2023-05-18 17:02:40 +0530

    Add files via upload

---
+++ bm1.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:07:46 05/12/2023 
+// Design Name: 
+// Module Name:    bm1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module bm1(
+  input [3:0] multiplicand,
+  input [3:0] multiplier,
+  output [15:0] product
+);
+
+  wire signed [15:0] signed_product;
+  wire [15:0] unsigned_product;
+
+  assign signed_product = multiplicand * multiplier;
+  assign unsigned_product = (signed_product < 0) ? ~signed_product + 1 : signed_product;
+
+  assign product = {(signed_product < 0) ? unsigned_product + 128 : unsigned_product};
+
+endmodule
+
---
+++ bm4.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:35:04 05/12/2023 
+// Design Name: 
+// Module Name:    bm4 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module bm4(X, Y, Z);
+      input signed [3:0] X, Y;
+       output signed [7:0] Z;
+       reg signed [7:0] Z;
+       reg [1:0] temp;
+       integer i;
+       reg E1;
+       reg [3:0] Y1;
+       always @ (X, Y)
+       begin
+       Z = 8'd0;
+       E1 = 1'd0;
+       for (i = 0; i < 4; i = i + 1)
+       begin
+       temp = {X[i], E1};
+		 Y1 = - Y;
+  case (temp)
+       2'd2 : Z [7 : 4] = Z [7 : 4] + Y1;
+       2'd1 : Z [7 : 4] = Z [7 : 4] + Y;
+       default : begin end
+       endcase
+       Z = Z >> 1;
+       
+       Z[7] = Z[6];
+       
+      
+       E1 = X[i];
+           end
+       if (Y == 4'd8)
+      
+      
+           begin
+               Z = - Z;
+           end
+      
+       end
+      
+     
+endmodule 
